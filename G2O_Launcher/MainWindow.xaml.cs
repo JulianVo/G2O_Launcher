2cffb4576775b26354cd3f9d1af687f05c2c35dd
Julian Vogel <Julian.Vogel1@gmx.net>
2016-08-07 14:25:41 +0200

    Created the UI project
    Created the UI project. Started the creation of the ui layout

---
+++ G2O_Launcher/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace G2O_Launcher
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseLeftButtonDown(e);
+
+            // Begin dragging the window
+            DragMove();
+        }
+    }
+}
