d13af81f9b764c4d28e13d03a16f76766e6016f7
Julian Vogel <Julian.Vogel1@gmx.net>
2016-10-15 15:08:24 +0200

    Added xml documentation to the UpdateResponse class.

--- G2O_Launcher/Updater/UpdateResponse.cs
+++ G2O_Launcher/Updater/UpdateResponse.cs
@@ -1,14 +1,35 @@
-﻿// // --------------------------------------------------------------------------------------------------------------------
-// // <copyright file="UpdateResponse.cs" company="Gothic Online Project">
-// //   
-// // </copyright>
-// // --------------------------------------------------------------------------------------------------------------------
+﻿//  --------------------------------------------------------------------------------------------------------------------
+//  <copyright file="UpdateResponse.cs" company="Gothic Online Project">
+//  Copyright (C) <2016>  <Julian Vogel>
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//  -
+//  This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+// -
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http:www.gnu.org/licenses/>.
+//  </copyright>
+//  -------------------------------------------------------------------------------
 namespace G2O_Launcher.Updater
 {
+    /// <summary>
+    ///     Class that is used for deserializing the response to the update request.
+    /// </summary>
     public class UpdateResponse
     {
-        public int Code = -1;
+        /// <summary>
+        ///     Status code.
+        /// </summary>
+        public int Code { get; set; } = -1;
 
+        /// <summary>
+        ///     Link to the new update file.
+        /// </summary>
         public string Link { get; set; }
     }
 }
